0fcda6656496d6932c02c266f8262165348f7671
elakala <elisa.lauttamus@gmail.com>
2026-05-24 19:06:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -540,7 +540,7 @@ Asiakirja:
 
     url = (
         "https://generativelanguage.googleapis.com/v1beta/models/"
-        f"gemini-1.5-flash:generateContent?key={gemini_avain}"
+        f"gemini-2.0-flash:generateContent?key={gemini_avain}"
     )
 
     payload = {
